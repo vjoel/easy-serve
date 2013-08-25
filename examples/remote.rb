9c1735bff35e16169d197a8db75b90e1bd890ca6
Joel VanderWerf <vjoel@users.sourceforge.net>
2013-08-25 12:29:57 -0700

    example of #remote

---
+++ examples/remote.rb
@@ -0,0 +1,47 @@
+require 'easy-serve'
+
+host = ARGV.shift
+unless host
+  abort <<-END
+    Usage: #$0 [user@]hostname
+    The argument may be any destination accepted by ssh, including host aliases.
+  END
+end
+
+EasyServe.start do |ez|
+  log = ez.log
+  log.level = Logger::DEBUG
+  log.formatter = nil if $VERBOSE
+
+  ez.start_servers do
+    ez.server "simple-server", :tcp, '0.0.0.0', 0 do |svr|
+      log.debug {"starting server"}
+      Thread.new do
+        loop do
+          Thread.new(svr.accept) do |conn|
+            log.info "accepted connection from #{conn.inspect}"
+            conn.write "hello from #{log.progname}"
+            log.info "wrote greeting"
+            conn.close_write
+            log.info "trying to read from #{conn.inspect}"
+            log.info conn.read
+            conn.close
+          end
+        end
+      end
+    end
+  end
+  
+  # druby version
+  ez.remote "simple-server", host: host do |conn|
+    # this block runs locally, but calls methods on the remote using drb
+    log.progname = "druby remote on #{host}"
+    log.info "trying to read from #{conn.inspect}"
+    log.info conn.read
+      # note: conn is drb proxy to real conn on remote host, so after the
+      # string is read from the socket in the remote, it is then serialized
+      # by drb back to this (local) process. Don't do this in production!
+    conn.write "hello from #{log.progname}"
+    conn.close
+  end
+end
