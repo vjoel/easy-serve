e3eaeec92edd9d6a6fffcc2861faa3d13573526f
Joel VanderWerf <vjoel@users.sourceforge.net>
2013-08-26 15:52:10 -0700

    remote_eval works

--- examples/remote-eval.rb
+++ examples/remote-eval.rb
@@ -1,12 +1,56 @@
+require 'easy-serve/remote'
 
-  # eval version
-  ez.remote "simple-server", host: host, eval: %w{
+addr_there = ARGV.shift
+
+unless addr_there
+  abort <<-END
+
+    Usage: #$0 addr_there
+
+    The 'addr_there' is the remote address on which client code will run.
+    It must be a destination accepted by ssh, optionally including a user name:
+
+      [user@]hostname
+    
+    The 'hostname' may by any valid hostname or ssh alias.
+
+  END
+end
+
+EasyServe.start do |ez|
+  log = ez.log
+  log.level = Logger::INFO
+  log.formatter = nil if $VERBOSE
+
+  ez.start_servers do
+    ez.server "simple-server", :tcp, nil, 0 do |svr|
+      Thread.new do
+        loop do
+          Thread.new(svr.accept) do |conn|
+            log.info "accepted connection from #{conn.inspect}"
+            conn.write "hello from #{log.progname}"
+            log.info "wrote greeting"
+            conn.close_write
+            log.info "trying to read from #{conn.inspect}"
+            log.info "received: #{conn.read}"
+            conn.close
+          end
+        end
+      end
+    end
+  end
+
+  ez.remote "simple-server", host: addr_there, eval: %{
+    conn = conns[0]
     # this code is executed on the remote host, connected by conn, not drb
-    log.progname = "eval remote on #{host}"
-    log.info "trying to read from #{conn.inspect}"
-    log.info "received: #{conn.read}"
-    conn.write "hello from #{log.progname}"
+    log.progname = "eval remote on \#{host}"
+    log.info "trying to read from \#{conn.inspect}"
+    log.info "received: \#{conn.read}"
+    conn.write "hello from \#{log.progname}"
     conn.close
   }
-
-###  ez.remote "simple-server", host: host, dir: "", run: "", args: []
+  # Note use of \#{} to interpolate variables that are only available
+  # in the binding where the code is eval-ed. Alternately, use
+  #   eval: %Q{...}
+  # but then interpolation from this script is not posssible.
+end
--- lib/easy-serve/remote-eval.rb
+++ lib/easy-serve/remote-eval.rb
@@ -2,5 +2,62 @@ class EasyServe
   # useful simple cases in testing and in production, but long eval strings
   # can be hard to debug -- use _run instead
   def remote_eval *server_names, host: nil, **opts
+    ## passive option?
+    ## remote logfile option?
+
+    log.progname = "remote_eval #{host}"
+
+    IO.popen ["ssh", host, "ruby"], "w+" do |ssh|
+      ssh.puts %Q{
+        $stdout.sync = true
+        begin
+          require 'yaml'
+          require 'easy-serve'
+ 
+          class EasyServe
+            def binding_for_remote_eval conns, host, log
+              binding
+            end
+          end
+          
+          server_names = #{server_names.inspect}
+          servers = YAML.load(#{YAML.dump(servers).inspect})
+          log_level = #{log.level}
+          eval_string = #{opts[:eval].inspect}
+          host = #{host.inspect}
+          
+          EasyServe.start servers: servers do |ez|
+            log = ez.log
+            log.level = log_level
+            log.formatter = nil if $VERBOSE
+
+            ez.local *server_names do |*conns|
+              begin
+                eval eval_string, ez.binding_for_remote_eval(conns, host, log)
+              rescue => ex
+                puts "ez error", ex, ex.backtrace
+              end
+            end
+          end
+        rescue => ex
+          puts "ez error", ex, ex.backtrace
+        end
+      }
+      
+      ssh.close_write
+      result = ssh.gets
+      
+      if result
+        error = result[/ez error/]
+        if error
+          raise RemoteError, "error raised in remote: #{ssh.read}"
+        else
+          puts result
+          while s = ssh.gets
+            puts s
+          end
+        end
+      end
+    end
   end
 end
