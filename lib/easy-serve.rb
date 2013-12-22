ccc41aa38c01a25255e4b25f7dd4161e7764c02f
Joel VanderWerf <vjoel@users.sourceforge.net>
2013-12-22 11:57:01 -0800

    release 0.10
    * services_file can be remote, specified as [user@]host:path/to/file
    
    * tunneling improvements:
      * client-managed tunneling, set up without server assistance (-L vs -R)
        * see examples/tunnel
      * services_file carries more info to avoid manual edits when tunneling
      * workaround for tunneling with OpenSSH versions older than 6.0

--- lib/easy-serve.rb
+++ lib/easy-serve.rb
@@ -6,7 +6,7 @@ require 'fileutils'
 require 'easy-serve/service'
 
 class EasyServe
-  VERSION = "0.9"
+  VERSION = "0.10"
   
   class EasyFormatter < Logger::Formatter
     Format = "%s: %s: %s\n"
